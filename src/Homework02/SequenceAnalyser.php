cad0fe83ef322a46f8dc39f9fa4237d8bf78ad14
juhaszjt <juhaszjt@gmail.com>
2014-09-23 07:21:04 +0200

    SequenceAnalyser & SequenceAnalyserTest
    SequenceAnalyser & SequenceAnalyserTest

---
+++ src/Homework02/SequenceAnalyser.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 23/09/2014
+ * Time: 07:19
+ */
+
+class SequenceAnalyser
+{
+
+}
---
+++ test/Homework02/SequenceAnalyserTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 23/09/2014
+ * Time: 07:19
+ */
+
+namespace Tdd\Test\Homework02;
+
+
+class SequenceAnalyserTest extends \PHPUnit_Framework_TestCase
+{
+
+}
+ 
