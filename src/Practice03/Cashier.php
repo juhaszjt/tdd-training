dc98425eddf70602aec9997e04f38b99d94333a3
juhaszjt <juhaszjt@gmail.com>
2014-09-23 18:21:40 +0200

    test Cashier test 1

---
+++ src/Practice03/Cashier.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 23/09/2014
+ * Time: 18:13
+ */
+
+//class Cashier
+//{
+//
+//}
---
+++ test/Practice03/CashierSupermarketTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 23/09/2014
+ * Time: 18:03
+ */
+
+namespace TDD\Practice03;
+
+use TDD\Practice03\Cashier;
+
+class CashierSupermarketTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function testCalculate()
+    {
+        $cashier = new Cashier();
+    }
+}
+ 
