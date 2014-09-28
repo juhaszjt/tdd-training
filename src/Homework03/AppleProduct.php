d09bc81c4ad7b7eeaabc0adc3ddf4e75b9e51c2d
juhaszjt <juhaszjt@gmail.com>
2014-09-28 23:41:08 +0200

    3 extended Product classes

---
+++ src/Homework03/AppleProduct.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 28/09/2014
+ * Time: 23:38
+ */
+
+class AppleProduct extends Product
+{
+
+}
---
+++ src/Homework03/StarShipProduct.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 28/09/2014
+ * Time: 23:39
+ */
+
+class StarShipProduct extends Product
+{
+
+}
