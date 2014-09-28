e0cb99c150e5908eaaa33ab9e9f0a0e528fa03a6
juhaszjt <juhaszjt@gmail.com>
2014-09-28 22:41:24 +0200

    ProductTest class

---
+++ test/Homework03/ProductTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 28/09/2014
+ * Time: 22:32
+ */
+
+namespace Tdd\Test\Homework03;
+
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+
+}
+ 
