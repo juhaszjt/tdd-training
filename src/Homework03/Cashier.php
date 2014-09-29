de47778aa121c484f195337ebb7332bf7c59705f
juhaszjt <juhaszjt@gmail.com>
2014-09-30 00:18:45 +0200

    testCashierProductsExists pass

--- src/Homework03/Cashier.php
+++ src/Homework03/Cashier.php
@@ -10,5 +10,40 @@ namespace Tdd\Homework03;
 
 class Cashier
 {
+    private $appleProduct;
+    private $lightProduct;
+    private $starShipProduct;
+
+    public function __construct()
+    {
+       $this->appleProduct = new AppleProduct();
+       $this->lightProduct = new LightProduct();
+       $this->starShipProduct = new StarShipProduct();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAppleProduct()
+    {
+        return $this->appleProduct;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLightProduct()
+    {
+        return $this->lightProduct;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStarShipProduct()
+    {
+        return $this->starShipProduct;
+    }
+
 
 }
