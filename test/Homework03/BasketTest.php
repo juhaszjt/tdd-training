a68b05e5394c6df97508585e4a13685f3cdddfeb
juhaszjt <juhaszjt@gmail.com>
2014-09-29 22:22:11 +0200

    testBasketExists

---
+++ test/Homework03/BasketTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jani
+ * Date: 29/09/2014
+ * Time: 22:18
+ */
+
+namespace Tdd\Test\Homework03;
+
+
+class BasketTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBasketExists()
+    {
+        $basket = new Basket();
+    }
+}
+ 
